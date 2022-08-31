b68f5f8af3c31b7bbe9e43eed2b520e1fcd4cb87
emaaperez9 <emaa.perez@gmail.com>
2022-08-31 19:57:42 -0300

    Update pruebaServo.ino

--- pruebaServo/pruebaServo.ino
+++ pruebaServo/pruebaServo.ino
@@ -1,22 +1,41 @@
-#include <Servo.h>
+// the number of the LED pin
+const int ledPin = 16;  // 16 corresponds to GPIO16
+
+// setting PWM properties
+const int freq = 50;
+const int ledChannel = 0;
+const int resolution = 8;
  
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
- 
-int pos = 0;    // variable to store the servo position
- 
-void setup() {
-  Serial.begin(115200);
-  myservo.attach(26);  // attaches the servo on pin 13 to the servo object
+void setup(){
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
 }
  
-void loop() {
-        while (Serial.available()) {
-                // read the incoming byte:
-                pos = Serial.read();
-                Serial.println(pos);
-                myservo.write(pos);
-                
-        }
+void loop(){
+  for(int dutyCycle = 13; dutyCycle <= 20; dutyCycle++){   //Se posiciona a 
+    ledcWrite(ledChannel, dutyCycle);
+    delay(15);
+  }
 
+  // decrease the LED brightness
+  for(int dutyCycle = 20; dutyCycle >= 13; dutyCycle--){
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);   
+    delay(15);
+  }/*
+  for(int dutyCycle = 20; dutyCycle <= 27; dutyCycle++){   //Se posiciona a 
+    ledcWrite(ledChannel, dutyCycle);
+    delay(15);
+  }
+
+  // decrease the LED brightness
+  for(int dutyCycle = 27; dutyCycle >= 20; dutyCycle--){
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);   
+    delay(15);
+  }
+  */
 }
