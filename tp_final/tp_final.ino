9bb9bf4622007596bfad4af258e328b9008e5808
emaaperez9 <emaa.perez@gmail.com>
2022-11-23 23:57:44 -0300

    Update tp_final.ino

--- tp_final/tp_final.ino
+++ tp_final/tp_final.ino
@@ -8,6 +8,8 @@
 #define DISTANCIA_MINIMA 2.00
 #define DISTANCIA_MAXIMA 10.00
 #define OPTICO_PIN 4
+#define TRIG_PIN 5
+#define ECHO_PIN 18
 //#define SERVO_PIN 26 // ESP32 pin GIOP26 connected to servo motor
 
 //Servo servoMotor;
@@ -15,14 +17,15 @@
 
 //const int estado_tacho_1 = 15;
 int estado = 0;
-const int optico_pin = 4;
 uint8_t lectura_Optico = 1;
 uint8_t lectura_Capacitivo = 0 ;
 uint8_t lectura_Inductivo = 0;
-const int trigPin = 5;
-const int echoPin = 18;
+//const int trigPin = 5;
+//const int echoPin = 18;
 long duracion;
 float distancia_Cm;
+float prueba_distancia;
+int estado_optico;
 //float distanceInch;
 // the number of the LED pin
 const int ledPin = 16;  // 16 corresponds to GPIO16
@@ -35,14 +38,15 @@ const int resolution = 8;
 /*const int LOADCELL_DOUT_PIN = 16;
 const int LOADCELL_SCK_PIN = 4;*/
 
+float distancia();
 
 
 void setup() {
   Serial.begin(115200);
   rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output para medición de distancia
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  pinMode(optico_pin,INPUT);
+  pinMode(TRIG_PIN, OUTPUT); // Sets the trigPin as an Output para medición de distancia
+  pinMode(ECHO_PIN, INPUT); // Sets the echoPin as an Input
+  pinMode(OPTICO_PIN,INPUT);
 
   //ledcSetup(ledChannel, freq, resolution); // Configura el pwm en el pin 17
   
@@ -52,42 +56,43 @@ void setup() {
 
 
 void loop() {
-
-  digitalWrite(trigPin, LOW);
+  prueba_distancia = distancia();
+  Serial.println(prueba_distancia);
+ /* digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(TRIG_PIN, LOW);
   
   // Lee el echoPin, devuelve el tiempo de viaje de la onda de sonido en microsegundos
-  duration = pulseIn(echoPin, HIGH);
+  duracion = pulseIn(ECHO_PIN, HIGH);*/
   
-  // Calcula la distancia
-  distanceCm = duracion * SOUND_SPEED/2;
+ 
+ 
   
   // Convert to inches
  // distanceInch = distanceCm * CM_TO_INCH;
   
   // Prints the distance in the Serial Monitor
-  Serial.print("Distance (cm): ");
-  Serial.println(distancia_Cm);
+  
   /*Serial.print("Distance (inch): ");
   Serial.println(distanceInch);*/
   
  // estado = digitalRead(estado_tacho_1); 
-  estado_optico = digitalRead(optico_pin);
+ /* estado_optico = digitalRead(OPTICO_PIN);
 if(estado_optico == LOW)
   Serial.println("Detecta"); 
   else
   Serial.println("No detecta");// muestro estado del sensor óptico
-  
+  */
   
  // changing the LED brightness with PWM
  //   ledcWrite(ledChannel, 127);
  //   delay(15);
-
- while(distancia_Cm >= DISTANCIA_MINIMA && distancia_Cm < DISTANCIA_MAXIMA)
+ 
+Serial.println("NO Hay nada");
+ while(prueba_distancia >= DISTANCIA_MINIMA && prueba_distancia < DISTANCIA_MAXIMA)
  {
   if (lectura_Optico == 1 && lectura_Capacitivo == 0 && lectura_Inductivo == 0)
   {
@@ -110,36 +115,26 @@ if(estado_optico == LOW)
                                   Serial.println("Metal");
                                 }
                                 else {
-                                  Serial.println("Este material no se recicla")
+                                  Serial.println("Este material no se recicla");
                                 }
- }                                
+                                prueba_distancia =distancia();
+ }
+}
+
+
+float distancia(){
+    digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
   
+  // Lee el echoPin, devuelve el tiempo de viaje de la onda de sonido en microsegundos
+  duracion = pulseIn(ECHO_PIN, HIGH);
+  
+ 
+  distancia_Cm = duracion * SOUND_SPEED/2;
 
-
-
-
-
-/*
-if(estado == LOW)
-  Serial.println("Ocupado"); 
-  else
-  Serial.println("vacío");// muestro estado
-
-  scale.power_down();             // put the ADC in sleep mode
-  delay(1500);
-  scale.power_up();*/
-
-  /* // rotates from 0 degrees to 180 degrees
-    for (int pos = 0; pos <= 180; pos += 1) {
-    // in steps of 1 degree
-    servoMotor.write(pos);
-    delay(15); // waits 15ms to reach the position
-  }
-
-  // rotates from 180 degrees to 0 degrees
-  for (int pos = 180; pos >= 0; pos -= 1) {
-    servoMotor.write(pos);
-    delay(15); // waits 15ms to reach the position
-  }
-*/
+  return(distancia_Cm);
 }
