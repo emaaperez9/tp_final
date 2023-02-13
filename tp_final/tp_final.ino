9e67f33edc4dfa9fbb3320078070bab7ce568b31
emaaperez9 <emaa.perez@gmail.com>
2023-02-13 18:08:50 -0300

    Update tp_final.ino

--- tp_final/tp_final.ino
+++ tp_final/tp_final.ino
@@ -2,7 +2,7 @@
 Librerías
 **************************************************************************************/
 #include "HX711.h"
-//#include "soc/rtc.h"
+#include "soc/rtc.h"
 #include <WiFi.h>
 //#include <Servo.h>
 //define sound speed in cm/uS
@@ -37,9 +37,9 @@ int habilitar_Servo=0;
 int habilitar_tacho_1=0; //PLASTICO - METAL
 int habilitar_tacho_2=0; //CARTON - PAPEL
 int habilitar_tacho_3=0; //VIDRIO
-int posicion_tacho_1;
-int posicion_tacho_2;
-int posicion_tacho_3;
+uint8_t posicion_tacho_1;
+uint8_t posicion_tacho_2;
+uint8_t posicion_tacho_3;
 // setting PWM properties
 const int freq = 1000;
 const int ledChannel = 0;
@@ -58,7 +58,7 @@ Configuración ESP32
 *************************************************************/
 void setup() {
   Serial.begin(115200);
- // rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
+  rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
   initWiFi();
   pinMode(TRIG_PIN, OUTPUT); // Sets the trigPin as an Output para medición de distancia
   pinMode(ECHO_PIN, INPUT); // Sets the echoPin as an Input
@@ -78,80 +78,85 @@ void setup() {
 
 
 void loop() {
-  /*lectura_Distancia = distancia();
+  lectura_Distancia = distancia();
   Serial.println(lectura_Distancia);
-  delay(100);*/
+  delay(100);
   //Serial.println(distancia());
+
   lectura_Capacitivo = digitalRead(CAPACITIVO_PIN);
   Serial.print("Capacitivo: ");
   Serial.println(lectura_Capacitivo);
-  delay(100);
+  delay(500);
  
   lectura_Inductivo = digitalRead(INDUCTIVO_PIN);
   Serial.print("Inductivo: ");
   Serial.println(lectura_Inductivo);
-  delay(100);
+  delay(500);
 
   lectura_Optico = digitalRead(OPTICO_PIN);
   Serial.print("OPTICO_PIN: ");
   Serial.println(lectura_Optico);
-  delay(100);
-/*
-    lectura_Optico = digitalRead(TACHO_1_PIN);
+  delay(500);
+
+  posicion_tacho_1 = digitalRead(TACHO_1_PIN);
   Serial.print("TACHO_PIN_1: ");
   Serial.println(posicion_tacho_1);
-  delay(100);
+  delay(500);
 
-    lectura_Optico = digitalRead(TACHO_2_PIN);
+  posicion_tacho_2 = digitalRead(TACHO_2_PIN);
   Serial.print("TACHO_PIN_2: ");
   Serial.println(posicion_tacho_2);
-  delay(100);
-*/
-    lectura_Optico = digitalRead(TACHO_3_PIN);
+  delay(500);
+
+  posicion_tacho_3 = digitalRead(TACHO_3_PIN);
   Serial.print("TACHO_PIN_3: ");
   Serial.println(posicion_tacho_3);
-  delay(100);
+  delay(500);
   
-/* while(lectura_Distancia >= DISTANCIA_MINIMA && lectura_Distancia < DISTANCIA_MAXIMA)
+ while(lectura_Distancia >= DISTANCIA_MINIMA && lectura_Distancia < DISTANCIA_MAXIMA)
  {
-  //habilitar_Servo = 1;
+  
   if (lectura_Optico == 1 && lectura_Capacitivo == 0 && lectura_Inductivo == 0)
   {
     Serial.println("PLASTICO"); 
     habilitar_tacho_1 = 1;
     break;
   }
-  else if (lectura_Optico == 0 && lectura_Capacitivo == 0 && lectura_Inductivo == 0)
+  else if (lectura_Optico == 0 && lectura_Capacitivo == 0 && lectura_Inductivo == 0)// el optico 0 detecta ppapel/carton!!!
           {
             Serial.println("PAPEL");
             habilitar_tacho_2 = 1;
+            habilitar_Servo = 1;
             break;
           }
           else if(lectura_Optico == 0 && lectura_Capacitivo == 1 && lectura_Inductivo == 0)
                    {
                      Serial.println("CARTON");
                      habilitar_tacho_2 = 1;
+                     habilitar_Servo = 1;
                      break;
                     }
-                    else if(lectura_Optico == 1 && lectura_Capacitivo == 1 && lectura_Inductivo == 0)
+                    else if(lectura_Optico == 1 && lectura_Capacitivo == 1 && lectura_Inductivo == 0)/// el optico en 1 indica que no detecta nada
                           {
                             Serial.println("VIDRIO");
                             habilitar_tacho_3 = 1;
+                            habilitar_Servo = 1;
                             break;
                             }
                             else if(lectura_Optico == 1 && lectura_Capacitivo == 1 && lectura_Inductivo == 1)
                                   {
                                     Serial.println("METAL");
                                     habilitar_tacho_1 = 1;
+                                    habilitar_Servo = 1;
                                     break;
                                   }
                                   else {
                                           Serial.println("Este material no se recicla");
-                                          //habilitar_Servo = 0;
+                                          habilitar_Servo = 0;
                                           break;
                                         }
  
-}*/
+}
 /********************************************************************************************************************
 Luego de determinar el tipo de residuo, debemos mover el tacho n a la posición indicada.
  Cada tacho tendra un final de carrera que indicará si este se encuentra en posición indicada para poder activar 
@@ -159,7 +164,6 @@ Luego de determinar el tipo de residuo, debemos mover el tacho n a la posición 
  final de carrera.
 Antes de 
 *********************************************************************************************************************/
-/*
 while(habilitar_Servo == 1)
 {
   if (habilitar_tacho_1 == 1 && posicion_tacho_1 == 1 && habilitar_Servo == 1)
@@ -182,7 +186,7 @@ while(habilitar_Servo == 1)
                   }
   }
 
-}*/
+}
 }
 
 /*****************************************************************
